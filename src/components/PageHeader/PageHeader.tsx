3c5aa08dd5759ac32eca37f1466011517d917eda
Łukasz Klęba <lukasz.kleba99@onet.pl>
2022-06-20 16:16:15 +0200

    create basic view of page header (beta version) and creates LoginView which handle login action with api and receives user data

--- src/components/PageHeader/PageHeader.tsx
+++ src/components/PageHeader/PageHeader.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import './PageHeader.css';
+import {Link} from "react-router-dom";
 
 export const PageHeader = () => {
     return <header>
         <h1>OPEN MIND LIBRARY</h1>
-        <button ></button>
+        <Link to='/library/login'><button></button></Link>
     </header>
 }
--- src/views/LoginView/LoginView.tsx
+++ src/views/LoginView/LoginView.tsx
@@ -1,11 +1,59 @@
 import React, {useState} from "react";
+import {Link} from "react-router-dom";
 
 export const LoginView = () => {
     const [login,setLogin] = useState<string>("");
     const [password,setPassword] = useState<string>("");
 
-    return <>
+    const handleSubmit = async (e : React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const isPassCorrect = await fetch('http://localhost:3001/login',{
+            method : "POST",
+            headers : {
+                "Content-Type" : "application/json"
+            },
+            body : JSON.stringify({
+                login : login,
+                password : password
+            })
+        })
+
+        const firstResp : {log:boolean} = await isPassCorrect.json();
+
+        if (firstResp.log) {
+            const resp = await fetch('http://localhost:3001/user',{
+                method : "POST",
+                headers : {
+                    "Content-Type" : "application/json"
+                },
+                body : JSON.stringify({
+                    login : login,
+                    password : password
+                })
+            })
+            const userData = await resp.json();
+            console.log(userData);
+        }
+    }
+
+    return <form onSubmit={e=>handleSubmit(e)}>
         <h1>Log in</h1>
-        <p>You must have</p>
-    </>
+        <label htmlFor="login">
+            Login
+        </label>
+        <input
+            type="text"
+            name="login"
+            value={login}
+            onChange={e=>setLogin(e.target.value)}
+        />
+        <label htmlFor="password">Password</label>
+        <input
+            type="text"
+            name="password"
+            value={password}
+            onChange={e=>setPassword(e.target.value)}
+        />
+        <button type="submit">Submit</button>
+    </form>
 }
