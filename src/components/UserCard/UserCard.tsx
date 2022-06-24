7cf19c710dc1d39a340017ec1e08a433386bf4e4
Łukasz Klęba <lukasz.kleba99@onet.pl>
2022-06-24 20:56:29 +0200

    Create elements to UserCard component and add some style to it

--- src/components/UserCard/UserCard.tsx
+++ src/components/UserCard/UserCard.tsx
@@ -1,6 +1,59 @@
-import React from "react";
+import React, {useContext, useState} from "react";
 import "./UserCard.css";
+import {userContext} from "../../utils/UserContext";
+import image from "../../user2.png";
+import {TokenObject} from "../../utils/types";
+
 
 export const UserCard = () => {
-    return null;
+    const context = useContext(userContext);
+    const [token, setToken] = useState<TokenObject | null>(null);
+
+    const createToken = async (): Promise<void> => {
+        const resp = await fetch("http://localhost:3001/token");
+        const passes = await resp.json() as TokenObject | null;
+        setToken(passes);
+    }
+
+    return <section className="userCard">
+        <div className="userCard__imageDiv">
+            <img src={image} alt="" className="userCard__imageDiv--img"/>
+        </div>
+        <div className="userCard__userData">
+            <span>{context && `Name and surname : ${context.userState.name} ${context.userState.surname}`}</span>
+            <span>{context && `Address : ${context.userState.city}, ${context.userState.address}`}</span>
+            <span>{context && `Phone number : ${context.userState.phone}`}</span>
+            <span>{context && `Email : ${context.userState.email}`}</span>
+            {
+                context && context.userState.is_admin
+                    ? <button className="userCard__userData--createTokenButton" onClick={createToken}>
+                        Create admin token</button>
+                    : null
+            }
+
+            {
+                token && <div className="userCard__userData--tokenDiv">
+                    <table>
+                        <thead>
+                        <tr>
+                            <td>TOKEN ID</td>
+                            <td>LOGIN</td>
+                            <td>PASSWORD</td>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td>{token.id}</td>
+                            <td>{token.login}</td>
+                            <td>{token.password}</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+            }
+        </div>
+    </section>
 }
+
+
+//<a href="https://www.flaticon.com/free-icons/user" title="user icons">User icons created by Freepik - Flaticon</a>
